77bb9f6e3e4aa8b6965a35abff57c0f576378a66
lijiaxiang <2633575992@qq.com>
2023-12-16 00:00:34 +0800

    first commit

--- com/lugora/controller/HelloController.java
+++ com/lugora/controller/HelloController.java
@@ -3,5 +3,6 @@ package com.lugora.controller;
 public class HelloController {
     public static void main(String[] args) {
         System.out.println(456);
+        System.out.println(1);
     }
 }
